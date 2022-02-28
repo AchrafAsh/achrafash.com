2120a23e981508554e9648c968eb6725ad7641ee
achrafash <aitsidihammou.achraf@gmail.com>
2022-02-28 21:39:06 +0100

    :rocket: initial setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,33 @@
 /** @type {import('next').NextConfig} */
+
+;(function buildSitemap() {
+    if (process.env.NODE_ENV !== 'production') return
+
+    const sitemap = require('nextjs-sitemap-generator')
+    sitemap({
+        baseUrl: 'https://achrafash.com',
+        pagesDirectory: __dirname + '/pages',
+        targetDirectory: 'public/',
+        ignoredExtensions: ['js', 'ts', 'map', 'html'],
+        ignoredPaths: [
+            '[fallback]',
+            'posts',
+            'issues',
+            'threads',
+            'app',
+            '404',
+            'early-access'
+        ]
+    })
+})()
+
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    exportPathMap: function () {
+        return {
+            '/': { page: '/' }
+        }
+    }
 }
 
 module.exports = nextConfig
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    content: ['./pages/**/*.tsx', './src/components/**/*.tsx'],
+    theme: {
+        fontFamily: {
+            sans: [
+                'Inter',
+                'system-ui',
+                '-apple-system',
+                'Segoe UI',
+                'Roboto',
+                'sans-serif',
+                'Segoe UI',
+
+                'Helvetica',
+                'Arial',
+                'Apple Color Emoji',
+                'Segoe UI Emoji'
+            ]
+        },
+        extend: {}
+    },
+    plugins: []
+}
